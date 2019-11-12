f9a3909853f4ba2541fe6f8dc3f5c022745c9c30
Rebecca Drabenstott <rdrabens@cisco.com>
2019-11-11 22:15:12 -0500

    works, but messy and lots of room for improvement

--- bin/set-super-isf.sh
+++ bin/set-super-isf.sh
@@ -1,81 +1,103 @@
 #!/bin/bash
-# example: fourISF.sh 29 22 19 17 100 140 190 60 30
-#
-# input params:
+# example: fourISF.sh 80 100 60 30
+
+# super ISF
+function computeSuperIsf {
+
+	# just use smallest basal rate for now, can get from profile later
+	assumedBasalRate=1.1
+	strongestSafeSuperIsf=35
+	stolenMinutesPerMgdl=$(bc <<<"scale=2;0.75")
+	
+	targetBg=`jq .max_bg $profileFile`
+	bg=`jq ".[0].glucose" /root/myopenaps/monitor/glucose.json`
+	deltaBg=$(bc <<<"scale=2;$bg-$targetBg")
+	
+	stolenMinutes=$(bc <<<"scale=2;$deltaBg*$stolenMinutesPerMgdl")
+	desiredStolenCorrectionUnits=$(bc <<<"scale=2;$assumedBasalRate*$stolenMinutes/60")
+	superIsf=$(bc <<<"scale=2;$deltaBg / ( ($deltaBg / $isfNormal) + $desiredStolenCorrectionUnits)")
+	roundedSuperIsf=$(bc <<<"($superIsf+.5)/1")
+	
+	safeSuperIsf=$roundedSuperIsf
+	if ((roundedSuperIsf < strongestSafeSuperIsf)); then
+	  safeSuperIsf=$strongestSafeSuperIsf
+	fi
+	
+	# just for logging
+	normalCorrectionUnits=$(bc <<<"scale=2;$deltaBg / $isfNormal")
+	totalCorrectionUnits=$(bc <<<"scale=2;$deltaBg / $safeSuperIsf")
+	# actual may be less than desired because strongestSafeSuperIsf limited the stolen correction units
+	actualStolenCorrectionUnits=$(bc <<<"scale=2;$totalCorrectionUnits-$normalCorrectionUnits")
+	
+	echo "using constants isfNormal: $isfNormal, strongestSafeSuperIsf: $strongestSafeSuperIsf, assumedBasalRate: $assumedBasalRate, stolenMinutesPerMgdl: $stolenMinutesPerMgdl"
+	echo "bg is $bg, targetBg is $targetBg, deltaBg is $deltaBg"
+	echo "stolenMinutes is $stolenMinutes, superIsf is $superIsf, roundedSuperIsf is $roundedSuperIsf, safeSuperIsf is $safeSuperIsf"
+	echo "normalCorrectionUnits is $normalCorrectionUnits, desiredStolenCorrectionUnits is $desiredStolenCorrectionUnits, actualStolenCorrectionUnits is $actualStolenCorrectionUnits, totalCorrectionUnits is $totalCorrectionUnits"
+
+    return $safeSuperIsf
+}
+
 isfNormal=$1
-isfStrong=$2
-isfHyper=$3
-isfRage=$4
 which=""
-strongThreshold=$5
-hyperThreshold=$6
-rageThreshold=$7
-maxSMBMinutes=$8
-maxUAMSMBMinutes=$9
-if [ -z  "$9" ]; then
-    echo "usage: $0 <isfNormal> <isfStrong> <isfHyper> <isfRage> <strongThreshold> <hyperThreshold> <rageThreshold> <maxSMBMinutes> <maxUAMSMBMinutes>"
+strongThreshold=$2
+maxSMBMinutes=$3
+maxUAMSMBMinutes=$4
+if [ -z  "$4" ]; then
+    echo "usage: $0 <isfNormal> <strongThreshold> <maxSMBMinutes> <maxUAMSMBMinutes>"
     echo "error: $0 requires parameters, exiting."
     exit 1
 fi
+
 # constants:
 preferencesFile=/root/myopenaps/preferences.json
 profileFile=/root/myopenaps/settings/autotune.json
 profileFile2=/root/myopenaps/autotune/profile.json
-# debug:
-#preferencesFile=/root/test/preferences.json
-#profileFile=/root/test/autotune.json
-#profileFile2=/root/test/profile.json
-# more constants:
+profileFileWithTempTargets=/root/myopenaps/settings/profile.json
 preferencesTemp=/tmp/4preferences.json
 profileTemp=/tmp/4autotune.json
 # execute:
 date
 if [ -z  "$which" ]; then
    glucose=`jq ".[0].glucose" /root/myopenaps/monitor/glucose.json`
-   if [ $(bc <<< "$glucose >= $strongThreshold") -eq 1 ]; then
-      if [ $(bc <<< "$glucose >= $hyperThreshold") -eq 1 ]; then
-         if [ $(bc <<< "$glucose >= $rageThreshold") -eq 1 ]; then
-            which="rage"
-         else
-            which="hyper"
-         fi
-      else
-         which="strong"
-      fi
-   else
+   tempTargetSet=`jq ".bg_targets.targets[1].temptargetSet" /root/myopenaps/settings/profile.json`
+   echo "tempTargetSet=$tempTargetSet"
+   if [ "$tempTargetSet" = "true" ]; then
+      echo "using normal because temp target set"
       which="normal"
+   elif ((glucose < strongThreshold)); then
+      echo "using normal because glucose below strong threshold"
+      which="normal"
+   else
+      echo "using strong because glucose above strong threshold and no temp target set"
+      which="strong"
    fi
    echo "glucose=$glucose, using $which isf"
 fi
 
-jq ".sensitivity_raises_target = false | .maxSMBBasalMinutes = 1 | .maxUAMSMBBasalMinutes = 1" $preferencesFile > $preferencesTemp.normal
-jq ".sensitivity_raises_target = true | .maxSMBBasalMinutes = $maxSMBMinutes | .maxUAMSMBBasalMinutes = $maxUAMSMBMinutes" $preferencesFile > $preferencesTemp.strong
-jq ".sensitivity_raises_target = true | .maxSMBBasalMinutes = $maxSMBMinutes | .maxUAMSMBBasalMinutes = $maxUAMSMBMinutes" $preferencesFile > $preferencesTemp.hyper
-jq ".sensitivity_raises_target = true | .maxSMBBasalMinutes = $maxSMBMinutes | .maxUAMSMBBasalMinutes = $maxUAMSMBMinutes" $preferencesFile > $preferencesTemp.rage
+jq ".maxSMBBasalMinutes = 1 | .maxUAMSMBBasalMinutes = 1" $preferencesFile > $preferencesTemp.normal
+jq ".maxSMBBasalMinutes = $maxSMBMinutes | .maxUAMSMBBasalMinutes = $maxUAMSMBMinutes" $preferencesFile > $preferencesTemp.strong
 autosens=`jq .ratio ~/myopenaps/settings/autosens.json`
 echo "isfNormal = $isfNormal"
+
+isfStrong=$isfNormal
+if [ "$which" = "strong" ]; then
+   computeSuperIsf
+   isfStrong=$?
+fi
 echo "isfStrong = $isfStrong"
-echo "isfHyper = $isfHyper"
-echo "isfRage = $isfRage"
+
 echo "autosens=$autosens"
 isfScaledNormal=$(bc -l <<< "$isfNormal * $autosens")
 isfScaledStrong=$(bc -l <<< "$isfStrong * $autosens")
-isfScaledHyper=$(bc -l <<< "$isfHyper * $autosens")
-isfScaledRage=$(bc -l <<< "$isfRage * $autosens")
 
-// change sensitivity of third item in array which is my daytime sensitivity
+# change sensitivity of third item in array which is my daytime sensitivity
 jq ".sens = ${isfScaledNormal} | .isfProfile.sensitivities[2].sensitivity = ${isfScaledNormal}" $profileFile > $profileTemp.normal
 jq ".sens = ${isfScaledStrong} | .isfProfile.sensitivities[2].sensitivity = ${isfScaledStrong}" $profileFile > $profileTemp.strong
-jq ".sens = ${isfScaledHyper} | .isfProfile.sensitivities[2].sensitivity = ${isfScaledHyper}" $profileFile > $profileTemp.hyper
-jq ".sens = ${isfScaledRage} | .isfProfile.sensitivities[2].sensitivity = ${isfScaledRage}" $profileFile > $profileTemp.rage
 
 echo "isfScaledNormal = $isfScaledNormal"
 echo "isfScaledStrong = $isfScaledStrong"
-echo "isfScaledHyper = $isfScaledHyper"
-echo "isfScaledRage = $isfScaledRage"
 echo "using $which isf"
 
-# copy (change profile first to ensure ISF is weakened before target lowered):
 diff $profileTemp.$which $profileFile
 if [ $? != 0 ]
 then
@@ -93,4 +115,6 @@ then
 else
    echo "already using $which preferences"
 fi
-# done.
+
+
+
